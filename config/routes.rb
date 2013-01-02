9597877164d1e3143529535cae2a2ec17ed79ebe
Sara Tsutsumida <sara.tsutsumida@sleepygiant.com>
2013-01-01 20:43:43 -0800

    signing in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy ]
 
   root to: 'static_pages#home'
 
@@ -9,6 +10,8 @@ SampleApp::Application.routes.draw do
   match '/home',    to: 'static_pages#home'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
 
 
